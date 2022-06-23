54ba135fdec0f4e94bf957062ffa65b29326435b
Neerti <Neerti@users.noreply.github.com>
2022-06-23 16:24:26 -0400

    Continues working on VectorHex.

---
+++ Game/Code/Map/VectorHex/VectorHex.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Collections.Generic;
+
+namespace Hexagon.Map.VectorHex
+{
+	/// <summary>
+	/// Virtual structure that represents a specific coordinate in a hexagonal grid,
+	/// unbound to any particular map implementation. Based off of
+	/// https://www.redblobgames.com/grids/hexagons
+	/// </summary>
+	public readonly struct VectorHex : IEquatable<VectorHex>
+	{
+		/// <summary>
+		/// Creates a new VectorHex struct.
+		/// </summary>
+		/// <param name="q"></param>
+		/// <param name="r"></param>
+		/// <param name="s"></param>
+		/// <exception cref="ArgumentException">All three arguments must sum to zero to be valid.</exception>
+		public VectorHex(int q, int r, int s)
+		{
+			Q = q;
+			R = r;
+			S = s;
+
+			X = q;
+			Y = r + (q - (q & 1)) / 2;
+			
+			if (q + r + s != 0)
+			{
+				throw new ArgumentException("(Q + R + S) must equal zero.");
+			}
+		}
+
+		public VectorHex(int x, int y)
+		{
+			X = x;
+			Y = y;
+			
+			Q = x;
+			R = y - (x - (x & 1)) / 2;
+			S = -Q - R;
+
+			if (Q + R + S != 0)
+			{
+				throw new ArgumentException("(Q + R + S) must equal zero.");
+			}
+		}
+
+		public int Q { get; }
+		public int R { get; }
+		public int S { get; }
+		
+		public int X { get; }
+		
+		public int Y { get; }
+
+		public static readonly List<VectorHex> AdjacentHexes = new List<VectorHex>
+		{
+			new VectorHex(1, 0, -1), new VectorHex(1, -1, 0), new VectorHex(0, -1, 1),
+			new VectorHex(-1, 0, 1), new VectorHex(-1, 1, 0), new VectorHex(0, 1, -1)
+		};
+		
+		public static VectorHex Adjacent(int direction)
+		{
+			return AdjacentHexes[direction];
+		}
+		
+		public override string ToString()
+		{
+			return $@"VectorHex (Cubic: {Q}, {R}, {S}) (Cartesian: {X}, {Y})";
+		}
+
+		// Equality.
+		public bool Equals(VectorHex other)
+		{
+			return Q == other.Q && R == other.R && S == other.S;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is VectorHex other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hashCode = Q;
+				hashCode = (hashCode * 397) ^ R;
+				hashCode = (hashCode * 397) ^ S;
+				return hashCode;
+			}
+		}
+
+		public static bool operator ==(VectorHex a, VectorHex b) => a.Equals(b);
+
+		public static bool operator !=(VectorHex a, VectorHex b) => !(a == b);
+
+		
+		// Arithmetic.
+		public static VectorHex operator +(VectorHex addendA, VectorHex addendB)
+		{
+			return new VectorHex(
+				addendA.Q + addendB.Q,
+				addendA.R + addendB.R,
+				addendA.S + addendB.S
+				);
+		}
+
+		public static VectorHex operator -(VectorHex minuend, VectorHex subtrahend)
+		{
+			return new VectorHex(
+				minuend.Q - subtrahend.Q,
+				minuend.R - subtrahend.R,
+				minuend.S - subtrahend.S
+			);
+		}
+
+		public static VectorHex operator *(VectorHex multiplier, int multiplicand)
+		{
+			return new VectorHex(
+				multiplier.Q * multiplicand,
+				multiplier.R * multiplicand,
+				multiplier.S * multiplicand
+			);
+		}
+
+		public static VectorHex operator /(VectorHex dividend, int divisor)
+		{
+			return new VectorHex(
+				dividend.Q / divisor,
+				dividend.R / divisor,
+				dividend.S / divisor
+			);
+		}
+
+		/// <summary>
+		///	Returns a new <see cref="VectorHex"/> that is rotated 60 degrees counter-clockwise.
+		/// </summary>
+		/// <returns>The rotated vector.</returns>
+		public VectorHex RotateLeft()
+		{
+			return new VectorHex(-S, -Q, -R);
+		}
+
+		/// <summary>
+		///	Returns a new <see cref="VectorHex"/> that is rotated 60 degrees clockwise.
+		/// </summary>
+		/// <returns>The rotated vector.</returns>
+		public VectorHex RotateRight()
+		{
+			return new VectorHex(-R, -S, -Q);
+		}
+		
+		public int Length()
+		{
+			return (Math.Abs(Q) + Math.Abs(R) + Math.Abs(S)) / 2;
+		}
+
+		public int Distance(VectorHex other)
+		{
+			return (this - other).Length();
+		}
+
+	}
+	
+	
+}
---
+++ Game/Tests/VectorHexTests.cs
@@ -0,0 +1,68 @@
+using Xunit;
+using Hexagon.Map.VectorHex;
+
+namespace Hexagon.Tests
+{
+	public class VectorHexTests
+	{
+		// Equals()
+		[Fact]
+		public void Equality_ComparingSameCoordinates_ShouldBeEqual()
+		{
+			var a = new VectorHex(1, -1, 0);
+			var b = new VectorHex(1, -1, 0);
+			
+			Assert.Equal(a, b);
+		}
+		
+		[Fact]
+		public void Equality_ComparingDifferentCoordinates_ShouldNotBeEqual()
+		{
+			var a = new VectorHex(1, -1, 0);
+			var b = new VectorHex(2, -2, 0);
+			
+			Assert.NotEqual(a, b);
+		}
+		
+		[Fact]
+		public void Equality_CubicVsCartesian_ShouldBeEqual()
+		{
+			var a = new VectorHex(5, -3, -2);
+			var b = new VectorHex(5, -1);
+			
+			Assert.Equal(a, b);
+		}
+		
+		// Add()
+		[Fact]
+		public void Addition_AddingTwoVectors_ShouldBeEqual()
+		{
+			var a = new VectorHex(2, -2, 0);
+			var b = new VectorHex(2, -2, 0);
+			var expected = new VectorHex(4, -4, 0);
+			
+			Assert.Equal(expected, a + b);
+		}
+		
+		// Subtract()
+		[Fact]
+		public void Subtraction_SubtractingTwoVectors_ShouldBeEqual()
+		{
+			var a = new VectorHex(2, -2, 0);
+			var b = new VectorHex(2, -2, 0);
+			var expected = new VectorHex(4, -4, 0);
+			
+			Assert.Equal(expected, a + b);
+		}
+		
+		// Multiply()
+		[Fact]
+		public void Multiplication_MultiplyingTwoVectors_ShouldBeEqual()
+		{
+			var a = new VectorHex(2, -2, 0);
+			var expected = new VectorHex(4, -4, 0);
+			
+			Assert.Equal(expected, a * 2);
+		}
+	}
+}

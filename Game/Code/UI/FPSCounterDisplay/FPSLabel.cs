faf28b41c80cc12d651fb2bac1b3e74b07c03f08
Neerti <Neerti@users.noreply.github.com>
2022-11-15 23:38:37 -0500

    Improves ingame debug FPS/Memory readout.

--- Game/Code/UI/FPSCounterDisplay/FPSLabel.cs
+++ Game/Code/UI/FPSCounterDisplay/FPSLabel.cs
@@ -8,19 +8,23 @@ namespace Hexagon.Map.Visual.VisualCells
 	{
 		public override void _Process(float delta)
 		{
+			var currentProcess = System.Diagnostics.Process.GetCurrentProcess();
 			Text = $@"Time
 - FPS: {GetMonitor(Monitor.TimeFps)}
-- Frame Allowance (60Hz): {(60f / GetMonitor(Monitor.TimeFps)) * 100:F2}%
-- Frame Allowance (144Hz): {(144 / GetMonitor(Monitor.TimeFps)) * 100:F2}%
-- Process Time: {GetMonitor(Monitor.TimeProcess) * 1000:F2} ms
-- Phys. Process Time: {GetMonitor(Monitor.TimePhysicsProcess) * 1000:F2} ms
+- Frame Allowance (60Hz): {(60f / GetMonitor(Monitor.TimeFps)) * 100f:F2}%
+- Frame Allowance (144Hz): {(144f / GetMonitor(Monitor.TimeFps)) * 100f:F2}%
+- Process Time: {GetMonitor(Monitor.TimeProcess) * 1000f:F2} ms
+- Phys. Process Time: {GetMonitor(Monitor.TimePhysicsProcess) * 1000f:F2} ms
 
-Memory
-- Static: {(GetMonitor(Monitor.MemoryStatic) / 1024 / 1024):F2} MiB
-- Max Static: {(GetMonitor(Monitor.MemoryStaticMax) / 1024 / 1024):F2} MiB
-- Dynamic: {(GetMonitor(Monitor.MemoryDynamic) / 1024 / 1024):F2} MiB
-- Max Dynamic: {(GetMonitor(Monitor.MemoryDynamicMax) / 1024 / 1024):F2} MiB
-- Max Message Buffer: {(GetMonitor(Monitor.MemoryMessageBufferMax) / 1024 / 1024):F2} MiB
+Memory (Godot)
+- Static: {(GetMonitor(Monitor.MemoryStatic) / 1024f / 1024f):F2} MiB
+- Max Static: {(GetMonitor(Monitor.MemoryStaticMax) / 1024f / 1024f):F2} MiB
+- Dynamic: {(GetMonitor(Monitor.MemoryDynamic) / 1024f / 1024f):F2} MiB
+- Max Dynamic: {(GetMonitor(Monitor.MemoryDynamicMax) / 1024f / 1024f):F2} MiB
+- Max Message Buffer: {(GetMonitor(Monitor.MemoryMessageBufferMax) / 1024f / 1024f):F2} MiB
+
+Memory (C#)
+- Physical Memory Usage: {(currentProcess.WorkingSet64 / 1024f / 1024f):F2} MiB
 
 Objects
 - Object Count: {GetMonitor(Monitor.ObjectCount)}
